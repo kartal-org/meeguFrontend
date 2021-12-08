4d39a8708ce298ad6aa8a1be583ef7a5d609df7a
nathanmaru <nathanmaru7@gmail.com>
2021-12-08 21:24:16 +0800

    notification realtime

--- src/classroomModule/classroomUser/classroomResearcher/classroomManager/tabs/workspaces/pageManager/tabs/members/ResearcherWorkspaceMemberTab.js
+++ src/classroomModule/classroomUser/classroomResearcher/classroomManager/tabs/workspaces/pageManager/tabs/members/ResearcherWorkspaceMemberTab.js
@@ -1,4 +1,4 @@
-import { Avatar, Card, CardContent, Menu, Typography } from '@mui/material';
+import { Avatar, Button, Card, CardContent, Menu, Typography } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
@@ -10,6 +10,7 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import CardHolder from '../../../../../../../../../materialUI/components/reuseableComponents/cardHolder';
 
 const ResearcherWorkspaceMemberTab = () => {
 	const { id } = useParams();
@@ -43,38 +44,43 @@ const ResearcherWorkspaceMemberTab = () => {
 		formData.append('workspace', id);
 		dispatch(addMember(`/workspace/member`, formData));
 	}
+	console.log(members);
 	return (
 		<>
-			<div className='flex flex-row space-x-4 w-full  mt-2'>
-				<div className='flex w-full justify-end'>
-					<DialogComponent title='Add Members'>
-						<FormControl fullWidth>
-							<InputLabel id='demo-simple-select-label'>
-								Select From Classroom Member
-							</InputLabel>
-							<Select
-								labelId='demo-simple-select-label'
-								id='demo-simple-select'
-								value={memberSelected}
-								label='Select From Classroom Member'
-								onChange={handleChange}
-							>
-								{members2.map((val) => (
-									<MenuItem value={val.user.id}>{val.user.full_name}</MenuItem>
-								))}
-							</Select>
-						</FormControl>
-					</DialogComponent>
-				</div>
-				{members.members ? (
+			<div className='flex w-full justify-end'>
+				<DialogComponent button={<Button>Add Member</Button>} title='Add Members'>
+					<FormControl fullWidth>
+						<InputLabel id='demo-simple-select-label'>
+							Select From Classroom Member
+						</InputLabel>
+						<Select
+							labelId='demo-simple-select-label'
+							id='demo-simple-select'
+							value={memberSelected}
+							label='Select From Classroom Member'
+							onChange={handleChange}
+						>
+							{members2.map((val) => (
+								<MenuItem value={val.user.id}>{val.user.full_name}</MenuItem>
+							))}
+						</Select>
+					</FormControl>
+				</DialogComponent>
+			</div>
+			<div className='flex flex-row space-x-4'>
+				{members ? (
 					<>
-						{members.members.map((member) => (
+						{members.map((member) => (
 							<Card raised sx={{ width: '200px', borderRadius: '1rem' }}>
 								<CardContent className='flex flex-col w-full justify-center items-center space-y-3 '>
 									<Avatar
 										alt='Remy Sharp'
-										src={member.profileImage}
-										sx={{ height: '100px', width: '100px', border: '1px solid #808080' }}
+										src={member.user.profileImage}
+										sx={{
+											height: '100px',
+											width: '100px',
+											border: '1px solid #808080',
+										}}
 									/>
 									<div className='flex flex-col justify-center items-center w-full space-y-1'>
 										<Typography
@@ -83,18 +89,18 @@ const ResearcherWorkspaceMemberTab = () => {
 											variant='p'
 											component='div'
 										>
-											{member.name}
+											{member.user.full_name}
 										</Typography>
 										<Typography
 											className='font-semibold'
 											variant='body2'
 											color='text.secondary'
 										>
-											@{member.username}
+											@{member.user.username}
 										</Typography>
 									</div>
 									<Typography variant='body1' color='text.primary'>
-										{member.role}
+										{member.user.role}
 									</Typography>
 								</CardContent>
 							</Card>
--- src/materialUI/components/toolbar.js
+++ src/materialUI/components/toolbar.js
@@ -1,69 +1,70 @@
-import * as React from "react";
-import { styled, alpha } from "@mui/material/styles";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import InputBase from "@mui/material/InputBase";
-import Badge from "@mui/material/Badge";
-import MenuItem from "@mui/material/MenuItem";
-import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
-import SearchIcon from "@mui/icons-material/Search";
-import AccountCircle from "@mui/icons-material/AccountCircle";
-import MailIcon from "@mui/icons-material/Mail";
-import NotificationsIcon from "@mui/icons-material/Notifications";
-import MoreIcon from "@mui/icons-material/MoreVert";
-import { useHistory } from "react-router-dom";
-import TextField from "@mui/material/TextField";
-import InputAdornment from "@mui/material/InputAdornment";
-import { logout } from "../../store/authSlice";
-import { useSelector } from "react-redux";
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import Badge from '@mui/material/Badge';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MailIcon from '@mui/icons-material/Mail';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import MoreIcon from '@mui/icons-material/MoreVert';
+import { useHistory } from 'react-router-dom';
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import { logout } from '../../store/authSlice';
+import { useSelector } from 'react-redux';
+import Pusher from 'pusher-js';
 
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-const Search = styled("div")(({ theme }) => ({
-	position: "relative",
-	borderRadius: "1.5rem",
-	backgroundColor: "rgba(229, 231, 235, 1)",
-	borderStyle: "solid",
-	borderColor: "#838CFF",
-	border: "2px",
-	"&:hover": {
-		backgroundColor: "rgba(229, 231, 235, 1)",
+const Search = styled('div')(({ theme }) => ({
+	position: 'relative',
+	borderRadius: '1.5rem',
+	backgroundColor: 'rgba(229, 231, 235, 1)',
+	borderStyle: 'solid',
+	borderColor: '#838CFF',
+	border: '2px',
+	'&:hover': {
+		backgroundColor: 'rgba(229, 231, 235, 1)',
 	},
 	marginRight: theme.spacing(2),
 	marginLeft: 0,
-	width: "100%",
+	width: '100%',
 
-	[theme.breakpoints.up("sm")]: {
+	[theme.breakpoints.up('sm')]: {
 		marginLeft: theme.spacing(3),
-		width: "auto",
+		width: 'auto',
 	},
 }));
 
-const SearchIconWrapper = styled("div")(({ theme }) => ({
+const SearchIconWrapper = styled('div')(({ theme }) => ({
 	padding: theme.spacing(0, 2),
-	height: "100%",
-	position: "absolute",
-	pointerEvents: "none",
-	display: "flex",
-	alignItems: "center",
-	justifyContent: "center",
+	height: '100%',
+	position: 'absolute',
+	pointerEvents: 'none',
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'center',
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-	color: "inherit",
-	"& .MuiInputBase-input": {
+	color: 'inherit',
+	'& .MuiInputBase-input': {
 		padding: theme.spacing(1, 1, 1, 0),
 		// vertical padding + font size from searchIcon
 		paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-		transition: theme.transitions.create("width"),
-		width: "100%",
+		transition: theme.transitions.create('width'),
+		width: '100%',
 
-		[theme.breakpoints.up("md")]: {
-			width: "20ch",
+		[theme.breakpoints.up('md')]: {
+			width: '20ch',
 		},
 	},
 }));
@@ -79,11 +80,43 @@ export default function PrimarySearchAppBar(props) {
 	const isMobileNotifOpen = Boolean(mobileMoreAnchorEl);
 	let history = useHistory();
 
+	// For Real Time Messaging
+	let allNotification = [];
+	const [notifications, setNotifications] = React.useState([]);
+	const [dataCatched, setDataCatched] = React.useState();
+	React.useEffect(() => {
+		if (dataCatched) {
+			notifications.map((val) => {
+				allNotification.push(val);
+			});
+			allNotification.push(dataCatched);
+
+			setNotifications(allNotification);
+		}
+	}, [dataCatched]);
+	function catchData(data) {
+		setDataCatched(data);
+	}
+
+	React.useEffect(() => {
+		if (user) {
+			Pusher.logToConsole = true;
+			const pusher = new Pusher('ba5283fee85d5a9a7b86', {
+				cluster: 'ap1',
+			});
+			const channel = pusher.subscribe('notification');
+			channel.bind(user.username, function (data) {
+				console.log(data);
+				catchData(data);
+			});
+		}
+	}, [user]);
+
 	const handleProfileMenuOpen = (event) => {
 		setAnchorEl(event.currentTarget);
 	};
 	const handleNotificationsOpen = () => {
-		history.push("/notif");
+		history.push('/notif');
 	};
 
 	const handleMobileMenuClose = () => {
@@ -100,16 +133,16 @@ export default function PrimarySearchAppBar(props) {
 	};
 
 	const goToProfile = () => {
-		history.push("/profile?tab=published-works");
+		history.push('/profile?tab=published-works');
 	};
 	const goToLogout = () => {
-		history.push("/logout");
+		history.push('/logout');
 	};
 	const goToNotifications = () => {
-		history.push("/notif");
+		history.push('/notif');
 	};
 
-	const menuId = "primary-search-account-menu";
+	const menuId = 'primary-search-account-menu';
 	const renderMenu = (
 		<>
 			<Menu
@@ -133,9 +166,7 @@ export default function PrimarySearchAppBar(props) {
 						goToProfile();
 					}}
 				>
-					<div className="flex justify-center items-center">
-						{user && user.username}
-					</div>
+					<div className='flex justify-center items-center'>{user && user.username}</div>
 				</MenuItem>
 				<MenuItem
 					onClick={() => {
@@ -149,42 +180,34 @@ export default function PrimarySearchAppBar(props) {
 		</>
 	);
 
-	const mobileMenuId = "primary-search-account-menu-mobile";
+	const mobileMenuId = 'primary-search-account-menu-mobile';
 	const renderMobileMenu = (
 		<Menu
 			anchorEl={mobileMoreAnchorEl}
 			anchorOrigin={{
-				vertical: "top",
-				horizontal: "right",
+				vertical: 'top',
+				horizontal: 'right',
 			}}
 			id={mobileMenuId}
 			keepMounted
 			transformOrigin={{
-				vertical: "top",
-				horizontal: "right",
+				vertical: 'top',
+				horizontal: 'right',
 			}}
 			open={isMobileMenuOpen}
 			onClose={handleMobileMenuClose}
 		>
 			<MenuItem>
-				<IconButton
-					size="large"
-					aria-label="show 17 new notifications"
-					color="inherit"
-				>
-					<Badge badgeContent={0} color="error">
+				<IconButton size='large' aria-label='show 17 new notifications' color='inherit'>
+					<Badge badgeContent={0} color='error'>
 						<SearchIcon />
 					</Badge>
 				</IconButton>
 				<p>Search</p>
 			</MenuItem>
 			<MenuItem onClick={handleNotificationsOpen}>
-				<IconButton
-					size="large"
-					aria-label="show 17 new notifications"
-					color="inherit"
-				>
-					<Badge badgeContent={0} color="error">
+				<IconButton size='large' aria-label='show 17 new notifications' color='inherit'>
+					<Badge badgeContent={notifications.length} color='error'>
 						<NotificationsIcon />
 					</Badge>
 				</IconButton>
@@ -192,18 +215,16 @@ export default function PrimarySearchAppBar(props) {
 			</MenuItem>
 			<MenuItem onClick={handleProfileMenuOpen}>
 				<IconButton
-					size="large"
-					aria-label="account of current user"
-					aria-controls="primary-search-account-menu"
-					aria-haspopup="true"
-					color="inherit"
+					size='large'
+					aria-label='account of current user'
+					aria-controls='primary-search-account-menu'
+					aria-haspopup='true'
+					color='inherit'
 				>
 					<AccountCircle />
 				</IconButton>
 				{/* <p>Profile</p> */}
-				<div className="flex justify-center items-center">
-					{user && user.username}
-				</div>
+				<div className='flex justify-center items-center'>{user && user.username}</div>
 			</MenuItem>
 		</Menu>
 	);
@@ -211,12 +232,12 @@ export default function PrimarySearchAppBar(props) {
 	return (
 		<Box sx={{ flexGrow: 1 }}>
 			<AppBar
-				position="sticky"
+				position='sticky'
 				sx={{
-					backgroundColor: "#FFFF",
-					borderBottom: "1px solid",
-					borderBottomColor: "#e1e1e1",
-					color: "rgba(55, 65, 81, 1)",
+					backgroundColor: '#FFFF',
+					borderBottom: '1px solid',
+					borderBottomColor: '#e1e1e1',
+					color: 'rgba(55, 65, 81, 1)',
 				}}
 				elevation={0}
 			>
@@ -236,47 +257,44 @@ export default function PrimarySearchAppBar(props) {
 						sx={{ borderRadius: 4 }}
 						variant='outlined'
 					/> */}
-					<Search sx={{ display: { xs: "none", md: "flex" } }}>
-						<SearchIconWrapper sx={{ display: { xs: "none", md: "flex" } }}>
+					<Search sx={{ display: { xs: 'none', md: 'flex' } }}>
+						<SearchIconWrapper sx={{ display: { xs: 'none', md: 'flex' } }}>
 							<SearchIcon />
 						</SearchIconWrapper>
-						<StyledInputBase
-							placeholder="Search…"
-							inputProps={{ "aria-label": "search" }}
-						/>
+						<StyledInputBase placeholder='Search…' inputProps={{ 'aria-label': 'search' }} />
 					</Search>
-					<Box sx={{ display: { xs: "none", md: "flex" } }}>
+					<Box sx={{ display: { xs: 'none', md: 'flex' } }}>
 						<IconButton
-							size="large"
-							aria-label="show 17 new notifications"
+							size='large'
+							aria-label='show 17 new notifications'
 							onClick={handleNotificationsOpen}
-							color="inherit"
+							color='inherit'
 						>
-							<Badge badgeContent={0} color="error">
+							<Badge badgeContent={notifications.length} color='error'>
 								<NotificationsIcon />
 							</Badge>
 						</IconButton>
 
 						<IconButton
-							size="large"
-							edge="end"
-							aria-label="account of current user"
+							size='large'
+							edge='end'
+							aria-label='account of current user'
 							aria-controls={menuId}
-							aria-haspopup="true"
+							aria-haspopup='true'
 							onClick={handleProfileMenuOpen}
-							color="inherit"
+							color='inherit'
 						>
 							<AccountCircle />
 						</IconButton>
 					</Box>
-					<Box sx={{ display: { xs: "flex", md: "none" } }}>
+					<Box sx={{ display: { xs: 'flex', md: 'none' } }}>
 						<IconButton
-							size="large"
-							aria-label="show more"
+							size='large'
+							aria-label='show more'
 							aria-controls={mobileMenuId}
-							aria-haspopup="true"
+							aria-haspopup='true'
 							onClick={handleMobileMenuOpen}
-							color="inherit"
+							color='inherit'
 						>
 							<MoreIcon />
 						</IconButton>
